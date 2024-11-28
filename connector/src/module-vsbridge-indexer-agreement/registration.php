75cd6f6b5c49113667049006fbd0e1b2d541aee2
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-11-28 12:05:52 +0100

    DXP-1310 Remove unwanted divante modules

--- connector/src/module-vsbridge-indexer-agreement/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Divante_VsbridgeIndexerAgreement',
-    __DIR__
-);
--- connector/src/module-vsbridge-indexer-review/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Divante_VsbridgeIndexerReview',
-    __DIR__
-);
