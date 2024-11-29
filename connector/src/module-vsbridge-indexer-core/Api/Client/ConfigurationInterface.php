1dc36b298a59e24f774e193ae57cf61c368ff6b2
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-11-29 11:09:13 +0100

    DXP-1310 Add publishing edited products to StreamX

--- connector/src/module-vsbridge-indexer-core/Api/Client/ConfigurationInterface.php
+++ connector/src/module-vsbridge-indexer-core/Api/Client/ConfigurationInterface.php
@@ -1,50 +1,11 @@
-<?php
-/**
- * @package   Divante\VsbridgeIndexerCore
- * @author    Agata Firlejczyk <afirlejczyk@divante.pl>
- * @copyright 2019 Divante Sp. z o.o.
- * @license   See LICENSE_DIVANTE.txt for license details.
- */
+<?php declare(strict_types=1);
 
 namespace Divante\VsbridgeIndexerCore\Api\Client;
 
-/**
- * Interface ConfigurationInterface
- */
-interface ConfigurationInterface
-{
-    /**
-     * @return string
-     */
-    public function getHost(int $storeId);
+interface ConfigurationInterface {
+    public function getIngestionBaseUrl(int $storeId): string;
 
-    /**
-     * @return string
-     */
-    public function getPort(int $storeId);
+    public function getPagesSchemaName(int $storeId): string;
 
-    /**
-     * @return string
-     */
-    public function getScheme(int $storeId);
-
-    /**
-     * @return bool
-     */
-    public function isHttpAuthEnabled(int $storeId);
-
-    /**
-     * @return string
-     */
-    public function getHttpAuthUser(int $storeId);
-
-    /**
-     * @return string
-     */
-    public function getHttpAuthPassword(int $storeId);
-
-    /**
-     * @return array
-     */
-    public function getOptions(int $storeId);
+    public function getOptions(int $storeId): array;
 }
--- copy_connector_to_magento.sh
+++ copy_connector_to_magento.sh
@@ -1,3 +1,10 @@
 rm -rf magento/src/app/code/StreamX/Connector
 mkdir -p magento/src/app/code/StreamX/Connector
 cp -R connector/ magento/src/app/code/StreamX/Connector
+
+# remove connector files that magento doesn't need
+rm -rf magento/src/app/code/StreamX/Connector/vendor
+rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-core/test
+rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-core/vendor
+rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-catalog/test
+rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-catalog/vendor
