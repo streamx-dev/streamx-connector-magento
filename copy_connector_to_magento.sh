ebedf69c00b938c6330b6143adce2c2a6b80cd2c
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-11-29 15:19:06 +0100

    DXP-1310 Rename modules from divante/module-vsbridge-indexer-[name] to streamx-connector/[name]

--- copy_connector_to_magento.sh
+++ copy_connector_to_magento.sh
@@ -4,7 +4,7 @@ cp -R connector/ magento/src/app/code/StreamX/Connector
 
 # remove connector files that magento doesn't need
 rm -rf magento/src/app/code/StreamX/Connector/vendor
-rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-core/test
-rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-core/vendor
-rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-catalog/test
-rm -rf magento/src/app/code/StreamX/Connector/src/module-vsbridge-indexer-catalog/vendor
+rm -rf magento/src/app/code/StreamX/Connector/src/core/test
+rm -rf magento/src/app/code/StreamX/Connector/src/core/vendor
+rm -rf magento/src/app/code/StreamX/Connector/src/catalog/test
+rm -rf magento/src/app/code/StreamX/Connector/src/catalog/vendor
