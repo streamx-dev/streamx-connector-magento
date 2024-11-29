74113c1961e3497f48bbd8386e746ce2eca08064
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-11-29 11:09:13 +0100

    DXP-1310 Add publishing edited products to StreamX

---
+++ connector/src/module-vsbridge-indexer-core/Streamx/StreamxPublisherProvider.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Divante\VsbridgeIndexerCore\Streamx;
+
+use Divante\VsbridgeIndexerCore\Api\Client\BuilderInterface;
+use Psr\Log\LoggerInterface;
+use Streamx\Clients\Ingestion\Builders\StreamxClientBuilders;
+use Streamx\Clients\Ingestion\Publisher\Publisher;
+
+class StreamxPublisherProvider {
+
+    private LoggerInterface $logger;
+    private Publisher $publisher;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
+    public function buildPublisher(array $options) {
+        if (isset($this->publisher)) {
+            $this->logger->info("Reusing publisher");
+            return $this->publisher;
+        }
+
+        $this->logger->info("Creating new publisher with options: " . json_encode($options));
+
+        $ingestionBaseUrl = $options[ClientConfiguration::INGESTION_BASE_URL_FIELD];
+        $pagesChannelSchemaName = $options[ClientConfiguration::PAGES_SCHEMA_NAME_FIELD];
+
+        $ingestionClient = StreamxClientBuilders::create($ingestionBaseUrl)->build();
+        $this->publisher = $ingestionClient->newPublisher("pages", $pagesChannelSchemaName);
+
+        return $this->publisher;
+    }
+
+}
