869efd073b2d44c935a759c927959cbf297803e2
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-11-29 13:15:25 +0100

    DXP-1310 Remove remaining copyright javadocs and authors

--- connector/src/module-vsbridge-indexer-catalog/Plugin/Controller/Catalog/Adminhtml/Product/Action/AttributeSavePlugin.php
+++ connector/src/module-vsbridge-indexer-catalog/Plugin/Controller/Catalog/Adminhtml/Product/Action/AttributeSavePlugin.php
@@ -1,10 +1,4 @@
 <?php declare(strict_types=1);
-/**
- * @package Divante\VsbridgeIndexerCatalog
- * @author Marek Mularczyk <mmularczyk@divante.pl>
- * @copyright 2019 Divante Sp. z o.o.
- * @license See LICENSE_DIVANTE.txt for license details.
- */
 
 namespace Divante\VsbridgeIndexerCatalog\Plugin\Controller\Catalog\Adminhtml\Product\Action;
 
