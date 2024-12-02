ea8b650661a9d1b46f2406cb41e58a4ab065e11f
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-02 13:50:34 +0100

    DXP-1310 Enable passing channel name, channel schema and auth token, to produce StreamX Client

--- connector/src/core/Streamx/ClientConfiguration.php
+++ connector/src/core/Streamx/ClientConfiguration.php
@@ -2,14 +2,16 @@
 
 namespace StreamX\ConnectorCore\Streamx;
 
-use StreamX\ConnectorCore\Api\Client\ConfigurationInterface as ClientConfigurationInterface;
+use StreamX\ConnectorCore\Api\Client\ConfigurationInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 
-class ClientConfiguration implements ClientConfigurationInterface {
+class ClientConfiguration implements ConfigurationInterface {
     const STREAMX_CLIENT_CONFIG_XML_PREFIX = 'streamx_indexer_settings/streamx_client';
     const INGESTION_BASE_URL_FIELD = 'ingestion_base_url';
-    const PAGES_SCHEMA_NAME_FIELD = 'pages_schema_name';
+    const CHANNEL_NAME_FIELD = 'channel_name';
+    const CHANNEL_SCHEMA_NAME_FIELD = 'channel_schema_name';
+    const AUTH_TOKEN_FIELD = 'auth_token';
 
     private ScopeConfigInterface $scopeConfig;
 
@@ -20,7 +22,9 @@ class ClientConfiguration implements ClientConfigurationInterface {
     public function getOptions(int $storeId): array {
         return [
             self::INGESTION_BASE_URL_FIELD => $this->getIngestionBaseUrl($storeId),
-            self::PAGES_SCHEMA_NAME_FIELD => $this->getPagesSchemaName($storeId)
+            self::CHANNEL_NAME_FIELD => $this->getChannelName($storeId),
+            self::CHANNEL_SCHEMA_NAME_FIELD => $this->getChannelSchemaName($storeId),
+            self::AUTH_TOKEN_FIELD => $this->getAuthToken($storeId)
         ];
     }
 
@@ -28,11 +32,19 @@ class ClientConfiguration implements ClientConfigurationInterface {
         return $this->getConfigParam(self::INGESTION_BASE_URL_FIELD, $storeId);
     }
 
-    public function getPagesSchemaName(int $storeId): string {
-        return $this->getConfigParam(self::PAGES_SCHEMA_NAME_FIELD, $storeId);
+    public function getChannelName(int $storeId): string {
+        return $this->getConfigParam(self::CHANNEL_NAME_FIELD, $storeId);
     }
 
-    private function getConfigParam(string $configField, $storeId): string {
+    public function getChannelSchemaName(int $storeId): string {
+        return $this->getConfigParam(self::CHANNEL_SCHEMA_NAME_FIELD, $storeId);
+    }
+
+    public function getAuthToken(int $storeId): ?string {
+        return $this->getConfigParam(self::AUTH_TOKEN_FIELD, $storeId);
+    }
+
+    private function getConfigParam(string $configField, $storeId): ?string {
         $path = self::STREAMX_CLIENT_CONFIG_XML_PREFIX . '/' . $configField;
         return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE, $storeId);
     }
