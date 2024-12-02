4f422424d6073929db96d62b9392939796b1f726
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-02 15:08:12 +0100

    DXP-1310 Speed up deploying changes in connector to magento

--- copy_connector_to_magento.sh
+++ copy_connector_to_magento.sh
@@ -1,10 +1,8 @@
 rm -rf magento/src/app/code/StreamX/Connector
-mkdir -p magento/src/app/code/StreamX/Connector
-cp -R connector/ magento/src/app/code/StreamX/Connector
+mkdir -p magento/src/app/code/StreamX/Connector/src
 
-# remove connector files that magento doesn't need
-rm -rf magento/src/app/code/StreamX/Connector/vendor
+cp -R connector/src magento/src/app/code/StreamX/Connector
+cp -R connector/composer.json magento/src/app/code/StreamX/Connector
+
 rm -rf magento/src/app/code/StreamX/Connector/src/core/test
-rm -rf magento/src/app/code/StreamX/Connector/src/core/vendor
 rm -rf magento/src/app/code/StreamX/Connector/src/catalog/test
-rm -rf magento/src/app/code/StreamX/Connector/src/catalog/vendor
