b6729479888e8f01b4271856d3b86e0e597509af
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-02 13:50:34 +0100

    DXP-1310 Enable passing channel name, channel schema and auth token, to produce StreamX Client

--- connector/src/core/Api/Client/ConfigurationInterface.php
+++ connector/src/core/Api/Client/ConfigurationInterface.php
@@ -5,7 +5,11 @@ namespace StreamX\ConnectorCore\Api\Client;
 interface ConfigurationInterface {
     public function getIngestionBaseUrl(int $storeId): string;
 
-    public function getPagesSchemaName(int $storeId): string;
+    public function getChannelName(int $storeId): string;
+
+    public function getChannelSchemaName(int $storeId): string;
+
+    public function getAuthToken(int $storeId): ?string;
 
     public function getOptions(int $storeId): array;
 }
