c08f8374c7ea1835d51fed6de441107a733bb70a
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-03 12:51:26 +0100

    DXP-1310 Speed up starting magento machinery by updating config between downloading and installing

--- copy_connector_to_magento_and_reload.sh
+++ copy_connector_to_magento_and_reload.sh
@@ -1,9 +1,2 @@
 bash copy_connector_to_magento.sh
-
-cd magento
-bin/magento cache:clean
-bin/magento cache:flush
-bin/magento setup:upgrade
-bin/magento setup:di:compile
-
-cd ../
+bash reload_magento_modules.sh
