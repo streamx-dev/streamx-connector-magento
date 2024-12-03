800939c0ec3ff3bd2bf79620dc1875b304021b32
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-03 12:51:26 +0100

    DXP-1310 Speed up starting magento machinery by updating config between downloading and installing

--- install-magento-with-connector.sh
+++ install-magento-with-connector.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e # exit on 1st error
+
 ### PREREQUISITES:
 # - streamx-ingestion-php repository cloned as sibling of streamx-connector-magento in your filesystem
 # - Composer authentication to download Magento images is configured - see README.md
@@ -14,18 +17,22 @@ rm -rf magento
 mkdir magento
 cd magento
 
-### Install magento docker images - see https://github.com/markshust/docker-magento
-curl -s https://raw.githubusercontent.com/markshust/docker-magento/master/lib/onelinesetup | bash -s -- magento.test community 2.4.7-p3
+### Download magento docker images - see https://github.com/markshust/docker-magento
+curl -s https://raw.githubusercontent.com/markshust/docker-magento/master/lib/template | bash
+bin/download community 2.4.7-p3
 
 ### Remove unnecessary docker-magento's .git repo folder
 rm -rf .git
 
-# Increase innodb-buffer-pool-size to avoid warnings in logs
+### Increase innodb-buffer-pool-size to avoid warnings in logs
 sed -i '' 's|--max_allowed_packet|--innodb-buffer-pool-size=512M --max_allowed_packet|g' compose.yaml
 
-# Turn off Two Factor Auth for development purposes
-bin/composer require --dev markshust/magento2-module-disabletwofactorauth
-bin/magento module:enable MarkShust_DisableTwoFactorAuth
+### To avoid conflicts with StreamX, replace known ports that are used by both StreamX and Magento by default:
+# 8080: ingestion port in StreamX and phpmyadmin port in Magento
+sed -i '' 's/8080:80/8090:80/g' compose.dev.yaml
+
+### Install the magento docker machinery
+bin/setup magento.test
 
 ### Install sample data
 bin/magento sampledata:deploy
@@ -56,25 +63,12 @@ bin/composer config repositories.streamx-client path app/code/StreamX/Client
 bin/composer config repositories.streamx-connector path app/code/StreamX/Connector
 
 # Add streamx client / connector to Magento's composer.json file
-bin/composer require "streamx/ingestion-client"
-bin/composer require "streamx/magento-connector"
+# Also turn off Two Factor Auth for development purposes and enable gathering code coverage
+bin/composer require \
+  "streamx/ingestion-client" \
+  "streamx/magento-connector" \
+  "markshust/magento2-module-disabletwofactorauth"
 
-# Register the module in Magento's system and apply any database changes or setup scripts
-bin/magento setup:upgrade
-
-# Clear Magento's cache to ensure the changes are reflected
-bin/magento cache:clean
-bin/magento cache:flush
-
-### To avoid conflicts with StreamX, replace known ports that are used by both StreamX and Magento by default:
-# - 9200, 9300: used by opensearch in both places
-sed -i '' 's/9200:9200/9201:9200/g' compose.yaml
-sed -i '' 's/9300:9300/9301:9300/g' compose.yaml
-sed -i '' 's/OPENSEARCH_PORT=9200/OPENSEARCH_PORT=9201/g' env/opensearch.env
-
-# - 8080: ingestion port in StreamX and phpmyadmin port in the Magento
-sed -i '' 's/8080:80/8090:80/g' compose.dev.yaml
-
-### Restart Magento to load all changes
-bin/stopall
-bin/start
+# Enable all modules
+bin/magento module:enable --all
+cd ../ && bash reload_magento_modules.sh
---
+++ reload_magento_modules.sh
@@ -0,0 +1,8 @@
+cd magento
+
+bin/magento cache:clean
+bin/magento cache:flush
+bin/magento setup:upgrade
+bin/magento setup:di:compile
+
+cd ../
