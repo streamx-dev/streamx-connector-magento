b12574d7c66cb119eb53fc9ad716a85c369359aa
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-06 15:59:36 +0100

    DXP-1413 Make IndexOperations class lighter to leave only features needed by StreamX

--- connector/src/core/Api/IndexOperationInterface.php
+++ connector/src/core/Api/IndexOperationInterface.php
@@ -2,15 +2,11 @@
 
 namespace StreamX\ConnectorCore\Api;
 
-use Magento\Store\Api\Data\StoreInterface;
-
 interface IndexOperationInterface
 {
     public function executeBulk(int $storeId, BulkRequestInterface $bulk): BulkResponseInterface;
 
     public function deleteByQuery(int $storeId, array $params): void;
-
-    public function getIndex(StoreInterface $store): IndexInterface;
 
     public function createBulk(): BulkRequestInterface;
 
