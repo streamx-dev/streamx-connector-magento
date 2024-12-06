cbf33f7d14bba4e6d46a5eddebaf1fa30efd0ed0
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-06 13:44:49 +0100

    DXP-1413 Move to importing streamx-ingestion-php client from public repository instead of from local filesystem

--- copy_client_to_magento.sh
+++
@@ -1,8 +0,0 @@
-rm -rf magento/src/app/code/StreamX/Client
-mkdir -p magento/src/app/code/StreamX/Client
-cp -R streamx-ingestion-php/ magento/src/app/code/StreamX/Client
-
-# remove client files that magento doesn't need
-rm -rf magento/src/app/code/StreamX/Client/vendor
-rm -rf magento/src/app/code/StreamX/Client/target
-rm -rf magento/src/app/code/StreamX/Client/tests
