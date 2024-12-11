e6ccfbee770f5bfb16833470ff20bb4a3c2cad07
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-11 14:55:30 +0100

    DXP-1310 Unnest connector from subdir to root dir of the project

--- copy-connector-to-magento.sh
+++ copy-connector-to-magento.sh
@@ -1,8 +1,8 @@
 rm -rf magento/src/app/code/StreamX/Connector
 mkdir -p magento/src/app/code/StreamX/Connector/src
 
-cp -R connector/src magento/src/app/code/StreamX/Connector
-cp -R connector/composer.json magento/src/app/code/StreamX/Connector
+cp -R src magento/src/app/code/StreamX/Connector
+cp -R composer.json magento/src/app/code/StreamX/Connector
 
 rm -rf magento/src/app/code/StreamX/Connector/src/core/test
 rm -rf magento/src/app/code/StreamX/Connector/src/catalog/test
