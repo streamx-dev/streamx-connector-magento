56dd8aa1ce13b5adb4866ba1862c49f3ac85eb7c
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-11 13:21:30 +0100

    DXP-1310 Add test for publishing category that was edited via Magento API

--- connector/src/catalog/test/integration/AppEntityUpdateStreamxPublishTests/BaseAppEntityUpdateStreamxPublishTest.php
+++ connector/src/catalog/test/integration/AppEntityUpdateStreamxPublishTests/BaseAppEntityUpdateStreamxPublishTest.php
@@ -2,6 +2,8 @@
 
 namespace StreamX\ConnectorCatalog\test\integration\AppEntityUpdateStreamxPublishTests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use StreamX\ConnectorCatalog\test\integration\BaseStreamxPublishTest;
 use StreamX\ConnectorCatalog\test\integration\utils\MagentoIndexerOperationsExecutor;
 
@@ -10,11 +12,22 @@ use StreamX\ConnectorCatalog\test\integration\utils\MagentoIndexerOperationsExec
  */
 abstract class BaseAppEntityUpdateStreamxPublishTest extends BaseStreamxPublishTest {
 
-    protected const REST_API_BASE_URL = 'https://magento.test/rest/all/V1';
+    private const REST_API_BASE_URL = 'https://magento.test/rest/all/V1';
 
     protected abstract function indexerName(): string;
 
     protected function desiredIndexerMode(): string {
         return MagentoIndexerOperationsExecutor::UPDATE_ON_SAVE_DISPLAY_NAME;
+    }
+
+    protected function callRestApiEndpoint(string $relativeUrl, array $params): void {
+        $endpointUrl = self::REST_API_BASE_URL . '/' . $relativeUrl;
+        $jsonBody = json_encode($params);
+        $headers = ['Content-Type' => 'application/json; charset=UTF-8'];
+
+        $request = new Request('PUT', $endpointUrl, $headers, $jsonBody);
+        $httpClient = new Client([ 'verify' => false ]);
+        $response = $httpClient->sendRequest($request);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
