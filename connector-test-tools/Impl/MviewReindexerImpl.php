f0ab70a22890386ec8122dc56029c3325a41d015
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2024-12-27 15:57:50 +0100

    DXP-1310 Remove unused field

--- connector-test-tools/Impl/MviewReindexerImpl.php
+++ connector-test-tools/Impl/MviewReindexerImpl.php
@@ -3,22 +3,18 @@
 namespace StreamX\ConnectorTestTools\Impl;
 
 use Exception;
-use Psr\Log\LoggerInterface;
 use StreamX\ConnectorCatalog\Cron\MView\StreamxIndexerMviewTrigger;
 use StreamX\ConnectorTestTools\Api\MviewReindexerInterface;
 
 class MviewReindexerImpl implements MviewReindexerInterface {
 
-    private LoggerInterface $logger;
     private array $streamxIndexers = [];
 
     public function __construct(
-        LoggerInterface $logger,
         StreamxIndexerMviewTrigger $productIndexer,
         StreamxIndexerMviewTrigger $categoryIndexer,
         StreamxIndexerMviewTrigger $attributeIndexer
     ) {
-        $this->logger = $logger;
         $this->streamxIndexers[$productIndexer->getIndexerViewId()] = $productIndexer;
         $this->streamxIndexers[$categoryIndexer->getIndexerViewId()] = $categoryIndexer;
         $this->streamxIndexers[$attributeIndexer->getIndexerViewId()] = $attributeIndexer;
