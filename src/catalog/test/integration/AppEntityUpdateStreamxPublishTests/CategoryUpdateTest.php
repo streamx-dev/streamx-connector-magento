a0ae45369ec41f8faa99ff4173aad1d6068710e4
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-01-08 10:56:01 +0100

    DXP-1310 Test also categories with subcategories

--- src/catalog/test/integration/AppEntityUpdateStreamxPublishTests/CategoryUpdateTest.php
+++ src/catalog/test/integration/AppEntityUpdateStreamxPublishTests/CategoryUpdateTest.php
@@ -17,8 +17,8 @@ class CategoryUpdateTest extends BaseAppEntityUpdateTest {
     /** @test */
     public function shouldPublishCategoryEditedUsingMagentoApplicationToStreamx() {
         // given
-        $categoryOldName = 'Watches';
-        $categoryNewName = 'Name modified for testing';
+        $categoryOldName = 'Gear';
+        $categoryNewName = 'Gear Articles';
         $categoryId = $this->db->getCategoryId($categoryOldName);
 
         // and
@@ -32,56 +32,162 @@ class CategoryUpdateTest extends BaseAppEntityUpdateTest {
         try {
             $this->assertExactDataIsPublished($expectedKey,
                 '{
-                    "parent_id": 3,
-                    "path": "1\/2\/3\/6",
-                    "position": 3,
-                    "level": 3,
-                    "children_count": 0,
-                    "name": "Name modified for testing",
-                    "url_key": "name-modified-for-testing-6",
-                    "url_path": "gear\/watches.html",
+                    "parent_id": 2,
+                    "path": "1\/2\/3",
+                    "position": 4,
+                    "level": 2,
+                    "children_count": 3,
+                    "name": "Gear Articles",
+                    "display_mode": "PAGE",
+                    "url_key": "gear-articles-3",
+                    "url_path": "gear.html",
                     "is_active": true,
-                    "is_anchor": true,
+                    "is_anchor": false,
                     "include_in_menu": 1,
-                    "children": null,
-                    "id": 6,
-                    "slug": "name-modified-for-testing-6",
+                    "children": "4,5,6",
+                    "id": 3,
+                    "slug": "gear-articles-3",
                     "default_sort_by": "position",
                     "available_sort_by": [
                         "name",
                         "price",
                         "position"
                     ],
-                    "product_count": 9,
-                    "children_data": []
+                    "product_count": 46,
+                    "children_data": [
+                        {
+                            "parent_id": 3,
+                            "path": "1\/2\/3\/4",
+                            "position": 1,
+                            "level": 3,
+                            "children_count": 0,
+                            "children_data": [],
+                            "name": "Bags",
+                            "url_key": "bags-4",
+                            "url_path": "gear\/bags.html",
+                            "is_active": true,
+                            "is_anchor": true,
+                            "include_in_menu": 1,
+                            "product_count": 14,
+                            "id": 4,
+                            "slug": "bags-4"
+                        },
+                        {
+                            "parent_id": 3,
+                            "path": "1\/2\/3\/5",
+                            "position": 2,
+                            "level": 3,
+                            "children_count": 0,
+                            "children_data": [],
+                            "name": "Fitness Equipment",
+                            "url_key": "fitness-equipment-5",
+                            "url_path": "gear\/fitness-equipment.html",
+                            "is_active": true,
+                            "is_anchor": true,
+                            "include_in_menu": 1,
+                            "product_count": 23,
+                            "id": 5,
+                            "slug": "fitness-equipment-5"
+                        },
+                        {
+                            "parent_id": 3,
+                            "path": "1\/2\/3\/6",
+                            "position": 3,
+                            "level": 3,
+                            "children_count": 0,
+                            "children_data": [],
+                            "name": "Watches",
+                            "url_key": "watches-6",
+                            "url_path": "gear\/watches.html",
+                            "is_active": true,
+                            "is_anchor": true,
+                            "include_in_menu": 1,
+                            "product_count": 9,
+                            "id": 6,
+                            "slug": "watches-6"
+                        }
+                    ]
                 }'
             );
         } finally {
             self::renameCategory($categoryId, $categoryOldName);
             $this->assertExactDataIsPublished($expectedKey,
                 '{
-                    "parent_id": 3,
-                    "path": "1\/2\/3\/6",
-                    "position": 3,
-                    "level": 3,
-                    "children_count": 0,
-                    "name": "Watches",
-                    "url_key": "watches-6",
-                    "url_path": "gear\/watches.html",
+                    "parent_id": 2,
+                    "path": "1\/2\/3",
+                    "position": 4,
+                    "level": 2,
+                    "children_count": 3,
+                    "name": "Gear",
+                    "display_mode": "PAGE",
+                    "url_key": "gear-3",
+                    "url_path": "gear.html",
                     "is_active": true,
-                    "is_anchor": true,
+                    "is_anchor": false,
                     "include_in_menu": 1,
-                    "children": null,
-                    "id": 6,
-                    "slug": "watches-6",
+                    "children": "4,5,6",
+                    "id": 3,
+                    "slug": "gear-3",
                     "default_sort_by": "position",
                     "available_sort_by": [
                         "name",
                         "price",
                         "position"
                     ],
-                    "product_count": 9,
-                    "children_data": []
+                    "product_count": 46,
+                    "children_data": [
+                        {
+                            "parent_id": 3,
+                            "path": "1\/2\/3\/4",
+                            "position": 1,
+                            "level": 3,
+                            "children_count": 0,
+                            "children_data": [],
+                            "name": "Bags",
+                            "url_key": "bags-4",
+                            "url_path": "gear\/bags.html",
+                            "is_active": true,
+                            "is_anchor": true,
+                            "include_in_menu": 1,
+                            "product_count": 14,
+                            "id": 4,
+                            "slug": "bags-4"
+                        },
+                        {
+                            "parent_id": 3,
+                            "path": "1\/2\/3\/5",
+                            "position": 2,
+                            "level": 3,
+                            "children_count": 0,
+                            "children_data": [],
+                            "name": "Fitness Equipment",
+                            "url_key": "fitness-equipment-5",
+                            "url_path": "gear\/fitness-equipment.html",
+                            "is_active": true,
+                            "is_anchor": true,
+                            "include_in_menu": 1,
+                            "product_count": 23,
+                            "id": 5,
+                            "slug": "fitness-equipment-5"
+                        },
+                        {
+                            "parent_id": 3,
+                            "path": "1\/2\/3\/6",
+                            "position": 3,
+                            "level": 3,
+                            "children_count": 0,
+                            "children_data": [],
+                            "name": "Watches",
+                            "url_key": "watches-6",
+                            "url_path": "gear\/watches.html",
+                            "is_active": true,
+                            "is_anchor": true,
+                            "include_in_menu": 1,
+                            "product_count": 9,
+                            "id": 6,
+                            "slug": "watches-6"
+                        }
+                    ]
                 }'
             );
         }
