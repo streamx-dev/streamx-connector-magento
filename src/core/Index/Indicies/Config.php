6cc9de72e3c2d66d71a244577fde8cad0c4990f4
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-01-09 09:15:36 +0100

    DXP-1310 Remove the now unused mapping classes

--- src/core/Index/Indicies/Config.php
+++ src/core/Index/Indicies/Config.php
@@ -4,26 +4,19 @@ namespace StreamX\ConnectorCore\Index\Indicies;
 
 use StreamX\ConnectorCore\Api\Index\TypeInterfaceFactory;
 use StreamX\ConnectorCore\Indexer\DataProviderProcessorFactory;
-use StreamX\ConnectorCore\Indexer\MappingProcessorFactory;
 
 class Config
 {
-    /**
-     * Factory used to build mapping types.
-     */
     private TypeInterfaceFactory $typeFactory;
     private DataProviderProcessorFactory $dataProviderFactoryProcessor;
-    private MappingProcessorFactory $mappingProviderProcessorFactory;
     private Config\Data $configData;
 
     public function __construct(
         Config\Data $configData,
         TypeInterfaceFactory $typeInterfaceFactory,
-        MappingProcessorFactory $mappingProcessorFactory,
         DataProviderProcessorFactory $dataProviderFactoryProcessor
     ) {
         $this->configData = $configData;
-        $this->mappingProviderProcessorFactory = $mappingProcessorFactory;
         $this->dataProviderFactoryProcessor = $dataProviderFactoryProcessor;
         $this->typeFactory = $typeInterfaceFactory;
     }
@@ -52,17 +45,10 @@ class Config
                     $this->dataProviderFactoryProcessor->get($dataProviderClass);
             }
 
-            $mapping = null;
-
-            if (isset($typeConfigData['mapping'][0])) {
-                $mapping = $this->mappingProviderProcessorFactory->get($typeConfigData['mapping'][0]);
-            }
-
             $types[$typeName] = $this->typeFactory->create(
                 [
                     'name' => $typeName,
-                    'dataProviders' => $dataProviders,
-                    'mapping' => $mapping,
+                    'dataProviders' => $dataProviders
                 ]
             );
         }
--- src/core/Index/Type.php
+++ src/core/Index/Type.php
@@ -5,29 +5,21 @@ namespace StreamX\ConnectorCore\Index;
 use Exception;
 use StreamX\ConnectorCore\Api\DataProviderInterface;
 use StreamX\ConnectorCore\Api\Index\TypeInterface;
-use StreamX\ConnectorCore\Api\MappingInterface;
 
 class Type implements TypeInterface
 {
     private string $name;
-    private MappingInterface $mapping;
     private array $dataProviders;
 
-    public function __construct(string $name, MappingInterface $mapping = null, array $dataProviders)
+    public function __construct(string $name, array $dataProviders)
     {
         $this->name = $name;
-        $this->mapping = $mapping;
         $this->dataProviders = $dataProviders;
     }
 
     public function getName(): string
     {
         return $this->name;
-    }
-
-    public function getMapping(): MappingInterface
-    {
-        return $this->mapping;
     }
 
     /**
