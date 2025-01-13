64bfaba7577a7a4473d4e7a1af7d20de030cb1c4
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2025-01-13 12:05:34 +0100

    DXP-1310 Remove unnecessary and unused inventory fields (#42)

--- src/catalog/Model/ResourceModel/Product/Inventory.php
+++ src/catalog/Model/ResourceModel/Product/Inventory.php
@@ -5,37 +5,25 @@ declare(strict_types=1);
 
 namespace StreamX\ConnectorCatalog\Model\ResourceModel\Product;
 
-use StreamX\ConnectorCatalog\Model\Inventory\Fields as InventoryFields;
 use Magento\Framework\App\ResourceConnection;
 use Magento\CatalogInventory\Api\StockConfigurationInterface;
 
 class Inventory
 {
+    private const requiredFields = [ 'product_id', 'qty' ];
+
     private StockConfigurationInterface $stockConfiguration;
     private ResourceConnection $resource;
-    private InventoryFields $inventoryFields;
 
     public function __construct(
         StockConfigurationInterface $stockConfiguration,
-        InventoryFields $fields,
         ResourceConnection $resourceModel
     ) {
-        $this->inventoryFields = $fields;
         $this->resource = $resourceModel;
         $this->stockConfiguration = $stockConfiguration;
     }
 
     public function loadInventory(array $productIds): array
-    {
-        return $this->getInventoryData($productIds, $this->inventoryFields->getRequiredColumns());
-    }
-
-    public function loadChildrenInventory(array $productIds): array
-    {
-        return $this->getInventoryData($productIds, $this->inventoryFields->getChildRequiredColumns());
-    }
-
-    private function getInventoryData(array $productIds, array $fields): array
     {
         $websiteId = $this->getWebsiteId();
         $connection = $this->resource->getConnection();
@@ -43,7 +31,7 @@ class Inventory
         $select = $connection->select()
             ->from(
                 ['main_table' => $this->resource->getTableName('cataloginventory_stock_item')],
-                $fields
+                self::requiredFields
             )->where('main_table.product_id IN (?)', $productIds);
 
         $joinConditionClause = [
