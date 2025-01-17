cc890359f2bbc3246558572801482b516da9dc25
Marek Czajkowski <marek.czajkowski@ds.pl>
2025-01-17 21:45:55 +0100

    nginx_test

--- add-rest-ingestion-to-magento-network.sh
+++ add-rest-ingestion-to-magento-network.sh
@@ -1,1 +1,4 @@
 docker network ls | grep magento_default | cut -d ' ' -f1 | xargs -I magento_network_id docker network connect magento_network_id rest-ingestion
+docker network ls | grep magento_default | cut -d ' ' -f1 | xargs -I magento_network_id docker network connect magento_network_id blueprint-web.webserver
+docker cp nginx.conf magento-app-1:/etc/nginx/conf.d/default.conf
+docker exec magento-app-1 nginx -s reload
