cb38a9f1473cf438ef28dd452e6408fea49fbb7d
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-01-29 12:58:52 +0100

    DXP-1568 Add instruction how to debug connector code

--- connector-test-tools/Impl/CoverageMeasurementTraits.php
+++ connector-test-tools/Impl/CoverageMeasurementTraits.php
@@ -5,12 +5,24 @@ namespace StreamX\ConnectorTestTools\Impl;
 trait CoverageMeasurementTraits {
 
     protected function executeWithCoverageMeasurement(callable $customCode): string {
-        xdebug_start_code_coverage();
+        $xdebugMode = ini_get('xdebug.mode');
+        $isCoverageMeasurementEnabled = str_contains($xdebugMode, 'coverage');
 
+        if ($isCoverageMeasurementEnabled) {
+            xdebug_start_code_coverage();
+
+            $customCode();
+
+            $coverage = xdebug_get_code_coverage();
+            xdebug_stop_code_coverage();
+            return json_encode($coverage);
+        } else {
+            return $this->executeWithoutCoverageMeasurement($customCode);
+        }
+    }
+
+    private function executeWithoutCoverageMeasurement(callable $customCode): string {
         $customCode();
-
-        $coverage = xdebug_get_code_coverage();
-        xdebug_stop_code_coverage();
-        return json_encode($coverage);
+        return json_encode([]);
     }
 }
