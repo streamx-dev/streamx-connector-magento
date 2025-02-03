e924aaf6f0428c22657630ab27cc6a9668232e23
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-02-03 15:16:41 +0100

    DXP-1567 Disallow full reindex of attributes indexer; use constants for indexed types

--- src/catalog/Model/Indexer/Action/Attribute.php
+++ src/catalog/Model/Indexer/Action/Attribute.php
@@ -17,7 +17,12 @@ class Attribute implements BaseAction {
 
     public function loadData(int $storeId = 1, array $attributeIds = []): Traversable {
         if (empty($attributeIds)) {
-            throw new RuntimeException(AttributeProcessor::INDEXER_ID . ' cannot be executed to process all attributes, please use other options.');
+            throw new RuntimeException(
+                'Indexation of all attributes is not supported.
+Available solutions:
+  - Use this indexer in Update By Schedule mode
+  - Reindex single attributes. Example of reindexing attribute with ID 93 from store 1:
+      bin/magento streamx:index streamx_attribute_indexer 1 93');
         }
 
         $lastAttributeId = 0;
