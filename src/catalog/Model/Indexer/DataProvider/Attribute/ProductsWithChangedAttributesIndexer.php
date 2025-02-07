58f4a107dddb707e87ce89a65235bd960419a133
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-02-07 12:14:32 +0100

    DXP-1568 Require strict types checking

--- src/catalog/Model/Indexer/DataProvider/Attribute/ProductsWithChangedAttributesIndexer.php
+++ src/catalog/Model/Indexer/DataProvider/Attribute/ProductsWithChangedAttributesIndexer.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace StreamX\ConnectorCatalog\Model\Indexer\DataProvider\Attribute;
 
--- src/catalog/Model/ResourceModel/Product/Configurable.php
+++ src/catalog/Model/ResourceModel/Product/Configurable.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace StreamX\ConnectorCatalog\Model\ResourceModel\Product;
 
