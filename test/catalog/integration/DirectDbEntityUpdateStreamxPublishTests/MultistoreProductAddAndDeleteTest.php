53fccbab3d52841a154b41f859e13daa33410c8b
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2025-02-13 14:49:54 +0100

    DXP-1633 Make sure store-specific product data will be selected and published (#156)

--- test/catalog/integration/DirectDbEntityUpdateStreamxPublishTests/MultistoreProductAddAndDeleteTest.php
+++ test/catalog/integration/DirectDbEntityUpdateStreamxPublishTests/MultistoreProductAddAndDeleteTest.php
@@ -20,6 +20,7 @@ class MultistoreProductAddAndDeleteTest extends BaseDirectDbEntityUpdateTest {
 
     private const DEFAULT_STORE_ID = 0; // comes with markshust/docker-magento
     private const STORE_1_ID = 1; // comes with markshust/docker-magento
+    private const STORE_2_ID = 2; // manually created according to instructions in the class comments
 
     protected function indexerName(): string {
         return ProductProcessor::INDEXER_ID;
@@ -27,11 +28,21 @@ class MultistoreProductAddAndDeleteTest extends BaseDirectDbEntityUpdateTest {
 
     /** @test */
     public function shouldPublishProductAddedDirectlyInDatabaseToStreamx_AndUnpublishDeletedProduct() {
-        // given
-        $productName = 'The minimal product';
-
-        // when: insert product as enabled for all stores by default, but disabled for store 1:
-        $productId = $this->insertProduct($productName, self::STORE_1_ID);
+        // given: insert product as enabled for all stores by default, but disabled for store 1:
+        $sku = (string) (new DateTime())->getTimestamp();
+        $productId = $this->insertProduct(
+            $sku,
+            [
+                self::DEFAULT_STORE_ID => 'Product name',
+                self::STORE_1_ID => 'Product name in first store',
+                self::STORE_2_ID => 'Product name in second store'
+            ],
+            [
+                self::DEFAULT_STORE_ID => Status::STATUS_ENABLED,
+                self::STORE_1_ID => Status::STATUS_DISABLED,
+                self::STORE_2_ID => Status::STATUS_ENABLED
+            ]
+        );
 
         // and
         $expectedKeyForStore1 = "pim:$productId";
@@ -39,15 +50,16 @@ class MultistoreProductAddAndDeleteTest extends BaseDirectDbEntityUpdateTest {
         $this->removeFromStreamX($expectedKeyForStore1, $expectedKeyForStore2);
 
         try {
-            // and
+            // when
             $this->reindexMview();
 
             // then
-            $this->assertExactDataIsPublished($expectedKeyForStore2, 'added-minimal-product.json', [
-                // mask variable parts (ids and generated sku)
-                '"id": [0-9]+' => '"id": 0',
-                '"sku": "[^"]+"' => '"sku": "[MASKED]"',
-                '"the-minimal-product-[0-9]+"' => '"the-minimal-product-0"'
+            $this->assertExactDataIsPublished($expectedKeyForStore2, 'added-multistore-product.json', [
+                // provide values for placeholders in the validation file
+                'SKU' => $sku,
+                123456789 => $productId,
+                'PRODUCT_NAME' => 'Product name in second store',
+                'PRODUCT_SLUG' => "product-name-in-second-store-$productId"
             ]);
 
             // and
@@ -62,35 +74,39 @@ class MultistoreProductAddAndDeleteTest extends BaseDirectDbEntityUpdateTest {
         }
     }
 
-    private function insertProduct(string $productName, int $storeIdWhereProductIsDisabled): int {
-        $sku = (string) (new DateTime())->getTimestamp();
-
-        $defaultStoreId = self::DEFAULT_STORE_ID;
+    private function insertProduct(string $sku, array $storeIdProductNameMap, array $storeIdProductStatusMap): int {
         $websiteId = 1;
         $attributeSetId = $this->db->getDefaultProductAttributeSetId();
         $nameAttrId = $this->db->getProductAttributeId('name');
         $statusAttrId = $this->db->getProductAttributeId('status');
-        $statusEnabled = Status::STATUS_ENABLED;
-        $statusDisabled = Status::STATUS_DISABLED;
 
+        // 1. Create product
         $productId = $this->db->insert("
             INSERT INTO catalog_product_entity (attribute_set_id, type_id, sku, has_options, required_options) VALUES
                 ($attributeSetId, 'simple', '$sku', FALSE, FALSE)
         ");
 
-        $this->db->executeAll(["
-            INSERT INTO catalog_product_entity_varchar (entity_id, attribute_id, store_id, value) VALUES
-                ($productId, $nameAttrId, $defaultStoreId, '$productName')
-        ", "
-            INSERT INTO catalog_product_entity_int (entity_id, attribute_id, store_id, value) VALUES
-                ($productId, $statusAttrId, $defaultStoreId, $statusEnabled)
-        ", "
-            INSERT INTO catalog_product_entity_int (entity_id, attribute_id, store_id, value) VALUES
-                ($productId, $statusAttrId, $storeIdWhereProductIsDisabled, $statusDisabled) -- override product status for specific store
-        ", "
+        // 2. Add the product to the website's catalog
+        $this->db->execute("
             INSERT INTO catalog_product_website (product_id, website_id) VALUES
                 ($productId, $websiteId)
-        "]);
+        ");
+
+        // 3. Set default and store-scoped names for the product
+        foreach ($storeIdProductNameMap as $storeId => $productName) {
+            $this->db->execute("
+               INSERT INTO catalog_product_entity_varchar (entity_id, attribute_id, store_id, value) VALUES
+                    ($productId, $nameAttrId, $storeId, '$productName')
+            ");
+        }
+
+        // 4. Set default and store-scoped statuses for the product
+        foreach ($storeIdProductStatusMap as $storeId => $productStatus) {
+            $this->db->execute("
+                INSERT INTO catalog_product_entity_int (entity_id, attribute_id, store_id, value) VALUES
+                    ($productId, $statusAttrId, $storeId, $productStatus)
+            ");
+        }
 
         return $productId;
     }
