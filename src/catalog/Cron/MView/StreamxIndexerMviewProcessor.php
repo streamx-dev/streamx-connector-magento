6dccdb6783cba76b60b1a140457777f955927dc4
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-02-14 11:11:06 +0100

    DXP-1633 Don't attempt to execute mview-based indexers from cron, if they are currently in the Update On Save mode

--- src/catalog/Cron/MView/StreamxIndexerMviewProcessor.php
+++ src/catalog/Cron/MView/StreamxIndexerMviewProcessor.php
@@ -3,6 +3,7 @@
 namespace StreamX\ConnectorCatalog\Cron\MView;
 
 use Exception;
+use Magento\Framework\Indexer\IndexerRegistry;
 use Magento\Framework\Mview\ViewInterface;
 use Psr\Log\LoggerInterface;
 use StreamX\ConnectorCatalog\Model\Indexer\AttributeProcessor;
@@ -13,10 +14,12 @@ class StreamxIndexerMviewProcessor {
 
     private LoggerInterface $logger;
     private ViewInterface $viewInterface;
+    private IndexerRegistry $indexerRegistry;
 
-    public function __construct(LoggerInterface $logger, ViewInterface $viewInterface) {
+    public function __construct(LoggerInterface $logger, ViewInterface $viewInterface, IndexerRegistry $indexerRegistry) {
         $this->logger = $logger;
         $this->viewInterface = $viewInterface;
+        $this->indexerRegistry = $indexerRegistry;
     }
 
     public function reindexProductMview(): void {
@@ -33,14 +36,17 @@ class StreamxIndexerMviewProcessor {
 
     /**
      * Triggers processing new data from _cl tables subscribed by the given indexer's MView
-     * @param string $indexerViewId view id (as in mview.xml file)
+     * @param string $indexerViewId view id (as in mview.xml file). Expecting that for each handled entity type, their view and indexer ids are the same
      * @throws Exception
      */
     public function reindexMview(string $indexerViewId): void {
-        $this->logger->info("Start processing mview for $indexerViewId");
-        $mView = $this->viewInterface->load($indexerViewId);
-        $mView->update();
-        $this->logger->info("Finished processing mview for $indexerViewId");
+        $indexer = $this->indexerRegistry->get($indexerViewId);
+        if ($indexer->isScheduled()) {
+            $this->logger->info("Start processing mview for $indexerViewId");
+            $mView = $this->viewInterface->load($indexerViewId);
+            $mView->update();
+            $this->logger->info("Finished processing mview for $indexerViewId");
+        }
     }
 
 }
