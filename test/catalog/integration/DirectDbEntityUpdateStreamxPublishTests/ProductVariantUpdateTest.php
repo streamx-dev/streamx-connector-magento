499eab81900f056488b80bf8ca53b6927a4ac826
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-02-20 12:21:19 +0100

    DXP-1672 Trigger additional parent/variants publishing

--- test/catalog/integration/DirectDbEntityUpdateStreamxPublishTests/ProductVariantUpdateTest.php
+++ test/catalog/integration/DirectDbEntityUpdateStreamxPublishTests/ProductVariantUpdateTest.php
@@ -15,61 +15,77 @@ class ProductVariantUpdateTest extends BaseDirectDbEntityUpdateTest {
     }
 
     /** @test */
-    public function shouldPublishProductWithVariantsEditedDirectlyInDatabaseToStreamx() {
+    public function shouldPublishParentProductAndAllVariants_WhenParentIsEditedDirectlyInDatabase() {
         // given
-        $nameOfProductToEdit = 'Chaz Kangeroo Hoodie';
-        $idOfProductToEdit = $this->db->getProductId($nameOfProductToEdit);
-        $newNameOfProductToEdit = "Name modified for testing, was $nameOfProductToEdit";
+        $parentProductName = 'Chaz Kangeroo Hoodie';
+        $parentProductId = $this->db->getProductId($parentProductName);
+
+        $childProducts = $this->db->getProductIdsAndNamesMap("$parentProductName-");
+        $this->assertCount(15, $childProducts);
 
         // and
-        $expectedPublishedKey = "pim:$idOfProductToEdit";
-        $unexpectedPublishedKey = 'pim:' . $this->db->getProductId('Chaz Kangeroo Hoodie-XL-Orange');
+        $expectedParentProductKey = "pim:$parentProductId";
+        $expectedChildProductsKeys = array_map(function ($childProductId) {
+            return "pim:$childProductId";
+        }, array_keys($childProducts));
 
-        self::removeFromStreamX($expectedPublishedKey, $unexpectedPublishedKey);
+        self::removeFromStreamX($expectedParentProductKey, ...$expectedChildProductsKeys);
 
         // when
-        $this->db->renameProduct($idOfProductToEdit, $newNameOfProductToEdit);
+        $this->db->renameProduct($parentProductId, "Name modified for testing, was $parentProductName");
 
         try {
             // and
             $this->reindexMview();
 
             // then
-            $this->assertExactDataIsPublished($expectedPublishedKey, 'edited-hoodie-product.json');
-            $this->assertDataIsNotPublished($unexpectedPublishedKey);
+            $this->assertExactDataIsPublished($expectedParentProductKey, 'edited-hoodie-product.json');
+            foreach ($childProducts as $childProductId => $childProductName) {
+                $publishedChildProduct = $this->downloadContentAtKey("pim:$childProductId");
+                $this->assertStringContainsString('"id":' . $childProductId, $publishedChildProduct);
+                $this->assertStringContainsString('"name":"' . $childProductName . '"', $publishedChildProduct);
+            }
         } finally {
-            $this->db->renameProduct($idOfProductToEdit, $nameOfProductToEdit);
+            $this->db->renameProduct($parentProductId, $parentProductName);
         }
     }
 
     /** @test */
-    public function shouldPublishParentOfProductVariantEditedUsingDirectlyInDatabaseToStreamx() {
+    public function shouldPublishVariantAndParentProduct_WhenVariantIsEditedUsingDirectlyInDatabase() {
         // given
-        $nameOfProductToEdit = 'Chaz Kangeroo Hoodie-XL-Orange';
-        $idOfProductToEdit = $this->db->getProductId($nameOfProductToEdit);
-        $newNameOfProductToEdit = "Name modified for testing, was $nameOfProductToEdit";
+        $childProductName = 'Chaz Kangeroo Hoodie-XL-Orange';
+        $childProductId = $this->db->getProductId($childProductName);
+
+        $parentProductName = 'Chaz Kangeroo Hoodie';
+        $parentProductId = $this->db->getProductId($parentProductName);
 
         // and
-        $expectedPublishedKey = 'pim:' . $this->db->getProductId('Chaz Kangeroo Hoodie');
-        $unexpectedPublishedKey = "pim:$idOfProductToEdit";
+        $expectedChildProductKey = "pim:$childProductId";
+        $expectedParentProductKey = "pim:$parentProductId";
+        $unexpectedChildProductKey = 'pim:' . $this->db->getProductId('Chaz Kangeroo Hoodie-L-Orange'); // a different child of the same parent product
 
-        self::removeFromStreamX($expectedPublishedKey, $unexpectedPublishedKey);
+        self::removeFromStreamX($expectedChildProductKey, $expectedParentProductKey, $unexpectedChildProductKey);
 
         // when
-        $this->db->renameProduct($idOfProductToEdit, $newNameOfProductToEdit);
+        $childProductModifiedName = "Name modified for testing, was $childProductName";
+        $this->db->renameProduct($childProductId, $childProductModifiedName);
 
         try {
             // and
             $this->reindexMview();
 
-            // then
-            $this->assertExactDataIsPublished($expectedPublishedKey, 'original-hoodie-product.json', [
-                '"' . $nameOfProductToEdit => '"' . $newNameOfProductToEdit,
-                '"' . SlugGenerator::slugify($nameOfProductToEdit) => '"' . SlugGenerator::slugify($newNameOfProductToEdit)
+            // then: expecting both products to be published (with modified name of the child product in both payloads). Other child should not be published
+            $this->assertExactDataIsPublished($expectedChildProductKey, 'original-hoodie-xl-orange-product.json', [
+                '"' . $childProductName => '"' . $childProductModifiedName,
+                '"' . SlugGenerator::slugify($childProductName) => '"' . SlugGenerator::slugify($childProductModifiedName)
             ]);
-            $this->assertDataIsNotPublished($unexpectedPublishedKey);
+            $this->assertExactDataIsPublished($expectedParentProductKey, 'original-hoodie-product.json', [
+                '"' . $childProductName => '"' . $childProductModifiedName,
+                '"' . SlugGenerator::slugify($childProductName) => '"' . SlugGenerator::slugify($childProductModifiedName)
+            ]);
+            $this->assertDataIsNotPublished($unexpectedChildProductKey);
         } finally {
-            $this->db->renameProduct($idOfProductToEdit, $nameOfProductToEdit);
+            $this->db->renameProduct($childProductId, $childProductName);
         }
     }
 }
