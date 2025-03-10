44b78f90fdc244b4b23967b440bff6a6de3ecd09
Stanisław Ruchlicki <165791712+stan-r-ds@users.noreply.github.com>
2025-03-10 15:29:56 +0100

    DXP-1767 Import test products while setting up local Magento test env

--- connector-test-tools/Api/EntityAddControllerInterface.php
+++ connector-test-tools/Api/EntityAddControllerInterface.php
@@ -18,17 +18,44 @@ interface EntityAddControllerInterface {
     /**
      * Adds a category
      * @param string $categoryName The display name for the new category
+     * @param int $parentCategoryId Parent category ID
      * @return int ID of the inserted category
      * @throws Exception
      */
-    public function addCategory(string $categoryName): int;
+    public function addCategory(string $categoryName, int $parentCategoryId): int;
 
     /**
-     * Adds a product attribute
+     * Adds a product attribute of type text
+     * @param string $attributeCode Internal code of the new attribute
+     * @return int ID of the inserted attribute
+     * @throws Exception
+     */
+    public function addTextAttribute(string $attributeCode): int;
+
+    /**
+     * Adds a product attribute that can have multiple values
+     * @param string $attributeCode Internal code of the new attribute
+     * @param string[] $values initial valid values of the attribute
+     * @return int ID of the inserted attribute
+     * @throws Exception
+     */
+    public function addMultiValuedAttribute(string $attributeCode, array $values): int;
+
+    /**
+     * Adds a product attribute that contains option
+     * @param string $attributeCode Internal code of the new attribute
+     * @param string[] $options valid options of the attribute
+     * @return int ID of the inserted attribute
+     * @throws Exception
+     */
+    public function addAttributeWithOptions(string $attributeCode, array $options): int;
+
+    /**
+     * Adds a product attribute and assigns it to the given product
      * @param string $attributeCode Internal code of the new attribute
      * @param int $productId ID of the product to add the attribute to
      * @return int ID of the inserted attribute
      * @throws Exception
      */
-    public function addAttribute(string $attributeCode, int $productId): int;
+    public function addAttributeAndAssignToProduct(string $attributeCode, int $productId): int;
 }
