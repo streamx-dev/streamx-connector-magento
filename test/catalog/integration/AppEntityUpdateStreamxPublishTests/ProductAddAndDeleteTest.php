277303e5021149acd73871931d9f1a5bd2ad691e
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-04-02 16:53:50 +0200

    Adjust to higher IDs

--- test/catalog/integration/AppEntityUpdateStreamxPublishTests/ProductAddAndDeleteTest.php
+++ test/catalog/integration/AppEntityUpdateStreamxPublishTests/ProductAddAndDeleteTest.php
@@ -30,7 +30,7 @@ class ProductAddAndDeleteTest extends BaseAppEntityUpdateTest {
         try {
             $publishedJson = $this->assertExactDataIsPublished($expectedKey, 'added-watch-product-without-custom-options.json', [
                 // mask variable parts (ids and generated sku)
-                '"id": "[0-9]{4}"' => '"id": "2659"',
+                '"id": "[0-9]{4,5}"' => '"id": "2659"',
                 '"sku": "[^"]+"' => '"sku": "1736952738"',
                 '"the-new-great-watch-[0-9]+"' => '"the-new-great-watch-2659"'
             ]);
