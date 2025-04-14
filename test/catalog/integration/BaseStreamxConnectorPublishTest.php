88ccc75c95f4174b3c8942135f6e14ef7e777c13
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-04-14 16:43:13 +0200

    DXP-1681 Perform faster serverside indexer mode switching

--- test/catalog/integration/BaseStreamxConnectorPublishTest.php
+++ test/catalog/integration/BaseStreamxConnectorPublishTest.php
@@ -12,10 +12,8 @@ use StreamX\ConnectorCatalog\test\integration\DirectDbEntityUpdateStreamxPublish
 use StreamX\ConnectorCatalog\test\integration\utils\CodeCoverageReportGenerator;
 use StreamX\ConnectorCatalog\test\integration\utils\EntityIds;
 use StreamX\ConnectorCatalog\test\integration\utils\MagentoEndpointsCaller;
-use StreamX\ConnectorCatalog\test\integration\utils\MagentoIndexerOperationsExecutor;
 use StreamX\ConnectorCatalog\test\integration\utils\MagentoLogFileUtils;
 use StreamX\ConnectorCatalog\test\integration\utils\MagentoMySqlQueryExecutor;
-use StreamX\ConnectorCatalog\test\integration\utils\MagentoOperationsExecutor;
 
 /**
  * @inheritDoc
@@ -36,13 +34,16 @@ abstract class BaseStreamxConnectorPublishTest extends BaseStreamxTest {
     protected const WEBSITE_2_CODE = 'second_website';
     protected const WEBSITE_2_STORE_CODE = 'store_for_second_website';
 
+    private const UPDATE_ON_SAVE_INDEXER_MODE = 'update-on-save';
+    private const UPDATE_BY_SCHEDULE_INDEXER_MODE = 'update-by-schedule';
+
     protected static bool $areTestsInitialized = false;
 
     private static array $initialIndexerModes;
 
     // every test operates on one or more indexes which share the same mode
     private static string $testedIndexerMode;
-    protected static array $testedIndexerNames;
+    protected static array $testedIndexerIds;
 
     protected static MagentoMySqlQueryExecutor $db;
     private MagentoLogFileUtils $logFileUtils;
@@ -69,8 +70,8 @@ abstract class BaseStreamxConnectorPublishTest extends BaseStreamxTest {
     }
 
     private static function loadInitialIndexerModes(): void {
-        foreach ([ProductProcessor::INDEXER_ID, CategoryProcessor::INDEXER_ID, AttributeProcessor::INDEXER_ID] as $indexerName) {
-            self::$initialIndexerModes[$indexerName] = MagentoIndexerOperationsExecutor::getIndexerMode($indexerName);
+        foreach ([ProductProcessor::INDEXER_ID, CategoryProcessor::INDEXER_ID, AttributeProcessor::INDEXER_ID] as $indexerId) {
+            self::$initialIndexerModes[$indexerId] = self::getIndexerMode($indexerId);
         }
     }
 
@@ -96,50 +97,63 @@ abstract class BaseStreamxConnectorPublishTest extends BaseStreamxTest {
     }
 
     private static function setIndexerModeInMagento(): void {
-        foreach (self::$testedIndexerNames as $testedIndexerName) {
-            if (self::$testedIndexerMode !== self::$initialIndexerModes[$testedIndexerName]) {
-                MagentoIndexerOperationsExecutor::setIndexerMode($testedIndexerName, self::$testedIndexerMode);
+        foreach (self::$testedIndexerIds as $testedIndexerId) {
+            if (self::$testedIndexerMode !== self::$initialIndexerModes[$testedIndexerId]) {
+                self::setIndexerMode($testedIndexerId, self::$testedIndexerMode);
             }
         }
     }
 
     private static function restoreIndexerModeInMagento(): void {
-        foreach (self::$testedIndexerNames as $testedIndexerName) {
-            $initialIndexerMode = self::$initialIndexerModes[$testedIndexerName];
+        foreach (self::$testedIndexerIds as $testedIndexerId) {
+            $initialIndexerMode = self::$initialIndexerModes[$testedIndexerId];
             if (self::$testedIndexerMode !== $initialIndexerMode) {
-                MagentoIndexerOperationsExecutor::setIndexerMode($testedIndexerName, $initialIndexerMode);
+                self::setIndexerMode($testedIndexerId, $initialIndexerMode);
             }
         }
     }
 
+    private static function getIndexerMode(string $indexerId): string {
+        return MagentoEndpointsCaller::call('indexer/mode/get', [
+            'indexerId' => $indexerId
+        ]);
+    }
+
+    private static function setIndexerMode(string $indexerId, string $mode): void {
+        MagentoEndpointsCaller::call('indexer/mode/set', [
+            'indexerId' => $indexerId,
+            'mode' => $mode
+        ]);
+    }
+
     private static function loadDesiredIndexerSettings(): void {
         $cls = new ReflectionClass(static::class);
-        self::$testedIndexerNames = self::getTestedIndexerNames($cls);
+        self::$testedIndexerIds = self::getTestedIndexerIds($cls);
         self::$testedIndexerMode = self::getTestedIndexerMode($cls);
     }
 
-    private static function getTestedIndexerNames(ReflectionClass $cls): array {
-        $indexerNames = [];
+    private static function getTestedIndexerIds(ReflectionClass $cls): array {
+        $indexerIds = [];
         $docComment = $cls->getDocComment();
         if (str_contains($docComment, '@UsesProductIndexer')) {
-            $indexerNames[] = ProductProcessor::INDEXER_ID;
+            $indexerIds[] = ProductProcessor::INDEXER_ID;
         }
         if (str_contains($docComment, '@UsesCategoryIndexer')) {
-            $indexerNames[] = CategoryProcessor::INDEXER_ID;
+            $indexerIds[] = CategoryProcessor::INDEXER_ID;
         }
         if (str_contains($docComment, '@UsesAttributeIndexer')) {
-            $indexerNames[] = AttributeProcessor::INDEXER_ID;
+            $indexerIds[] = AttributeProcessor::INDEXER_ID;
         }
-        return $indexerNames;
+        return $indexerIds;
     }
 
     private static function getTestedIndexerMode(ReflectionClass $cls): string {
         if ($cls->isSubclassOf(BaseAppEntityUpdateTest::class)) {
-            return MagentoIndexerOperationsExecutor::UPDATE_ON_SAVE_DISPLAY_NAME;
+            return self::UPDATE_ON_SAVE_INDEXER_MODE;
         }
         if ($cls->isSubclassOf(BaseDirectDbEntityUpdateTest::class)) {
             // Magento creates triggers to save db-level changes only when the scheduler is in the below mode:
-            return MagentoIndexerOperationsExecutor::UPDATE_BY_SCHEDULE_DISPLAY_NAME;
+            return self::UPDATE_BY_SCHEDULE_INDEXER_MODE;
         }
         throw new Exception("Cannot detect desired indexer mode for $cls");
     }
