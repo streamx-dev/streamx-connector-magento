6ba161778f3081b3520f08e17d61ffa066b25f3c
Stanisław Ruchlicki <stanislaw.ruchlicki@ds.pl>
2025-05-15 11:08:45 +0200

    Add info on test modes

--- test/catalog/integration/DirectDbEntityUpdateStreamxPublishTests/BaseDirectDbEntityUpdateTest.php
+++ test/catalog/integration/DirectDbEntityUpdateStreamxPublishTests/BaseDirectDbEntityUpdateTest.php
@@ -6,6 +6,11 @@ use StreamX\ConnectorCatalog\test\integration\BaseStreamxConnectorPublishTest;
 
 /**
  * @inheritdoc
+ *
+ * Base class for testing indexers in Update By Schedule mode.
+ * In this mode, Magento's MView feature is used, that tracks down changes in database tables.
+ * These tests insert/update/delete database rows directly and then manually trigger MView to verify if Product / Category data is published.
+ * In real world scenario, MView is triggered by Magento's cron system, according to schedule
  */
 abstract class BaseDirectDbEntityUpdateTest extends BaseStreamxConnectorPublishTest {
     const INDEXER_MODE = parent::UPDATE_BY_SCHEDULE;
